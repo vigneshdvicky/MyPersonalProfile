4a11e9d388b7760a1e762fbcb9292608e6e6df96
Vicky <vignesh@lucidindia.com>
2021-01-27 15:26:37 +0530

    Base commit for the project

---
+++ MyPersonalProfile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyPersonalProfile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
